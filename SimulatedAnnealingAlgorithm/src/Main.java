bb4b2ffecdcb2f5b261e70cb785e4053ec1348f3
Can Kozan <43638551+JMetalian@users.noreply.github.com>
2021-01-15 21:48:12 +0300

    Code style updated

--- SimulatedAnnealingAlgorithm/src/Main.java
+++ SimulatedAnnealingAlgorithm/src/Main.java
@@ -21,17 +21,22 @@ public class Main {
         double yFinal=yZero;
 
         Random ranat= new Random();
-        for (int i=1; i<=m;i++){
-            for (int j=1; j<=n;j++){
+        for (int i=1; i<=m;i++)
+        {
+            for (int j=1; j<=n;j++)
+            {
                 double xTemp=moveOpX(x); double yTemp=moveOpY(y);
                 if (func(xTemp,yTemp)<=func(x,y)){
                     x=xTemp;y=yTemp;
-                }else{
-                    if (ranat.nextGaussian()<=metroPol(func(xTemp,yTemp),func(x,y),t)){
+                }else
+                {
+                    if (ranat.nextGaussian()<=metroPol(func(xTemp,yTemp),func(x,y),t))
+                    {
                         x=xTemp;y=yTemp;
                     }
                 }
-                if(func(x,y)<=func(xFinal,yFinal)){
+                if(func(x,y)<=func(xFinal,yFinal))
+                {
                     xFinal=x; yFinal=y;
                 }
             }
@@ -42,24 +47,29 @@ public class Main {
         array[2]=func(xFinal,yFinal);
         return array;
     }
-    public static double moveOpX(double num){//Move Operator for X
+    public static double moveOpX(double num)
+    {//Move Operator for X
         Random random= new Random();
         double neigh = num+random.nextGaussian();
         return neigh;
     }
-    public static double moveOpY(double num2){//Move Operator Y
+    public static double moveOpY(double num2)
+    {//Move Operator Y
         Random random2= new Random();
         double neigh2 = num2+random2.nextGaussian();
         return neigh2;
     }
-    static double func(double x, double y) {//Function that is operated
+    static double func(double x, double y) 
+    {//Function that is operated
         return (4-2.1*Math.pow(x,2)+Math.pow(x,4)/3)*Math.pow(x,2)+x*y+(-4+4*Math.pow(y,2))*Math.pow(y,2);
     }
-    static double metroPol(double z1, double z2,double temperature){//Metropolis statement
+    static double metroPol(double z1, double z2,double temperature)
+    {//Metropolis statement
         double res = Math.pow(Math.E,-((z1-z2)/temperature));
         return res;
     }
-    public static void main(String[] args){//Main function
+    public static void main(String[] args)
+    {//Main function
         double[] arr = new double[3];
         arr = SimulatedAnnealing();
         System.out.println("X final is: "+ arr[0]);
